52bb9a0a0900837edf61e7217a26d1b491248e8c
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-23 12:30:17 +0000

    Implemented basic puzzle loader function (does not validate puzzles), and a basic test checking that it correctly loads a (known) valid puzzle.
    Also created pre-commit-confi.yaml file, including flake8 and pytest.
    I changed the max line length for flake8 to be 120 characters instead of 79.

---
+++ src/puzzleloading/loader.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def load_puzzle(filepath):
+    """
+    Function loads sudoku puzzles from text files to numpy arrays
+
+    The format for a sudoku puzzle just needs to be:
+    - 9 digits in a row (can be separated by any of the following separators: | + - , or a space " ")
+    - rows separated by new-lines
+    - rows can also be separated by lines containing only the separators | + - , or spaces " "
+    """
+
+    # ensuring the file provided is a text file
+    assert filepath[-4:] == ".txt"
+
+    with open(filepath, "r") as file:
+        text = file.read()
+
+    # remove separators (special characters), spaces and empty lines
+    text = text.replace("|", "").replace("+", "").replace("-", "").replace(",", "")
+    text = text.replace(" ", "")
+    text = text.replace("\n\n", "\n")
+
+    # convert to list of rows
+    rows = text.split("\n")
+    rows = [row for row in rows if row != ""]  # remove empty rows
+
+    # ------------
+    # Add functionality to validate puzzle
+    # ------------
+
+    # convert to array
+    puzzle = np.array([[int(char) for char in row] for row in rows])
+
+    return puzzle
---
+++ tests/test_loader.py
@@ -0,0 +1,24 @@
+import numpy as np
+from src.puzzleloading.loader import load_puzzle
+
+
+def test_load_puzzle():
+    """
+    Test load_puzzle
+    """
+    correct_puzzle = np.array(
+        [
+            [0, 0, 0, 0, 0, 7, 0, 0, 0],
+            [0, 0, 0, 0, 0, 9, 5, 0, 4],
+            [0, 0, 0, 0, 5, 0, 1, 6, 9],
+            [0, 8, 0, 0, 0, 0, 3, 0, 5],
+            [0, 7, 5, 0, 0, 0, 2, 9, 0],
+            [4, 0, 6, 0, 0, 0, 0, 8, 0],
+            [7, 6, 2, 0, 8, 0, 0, 0, 0],
+            [1, 0, 3, 9, 0, 0, 0, 0, 0],
+            [0, 0, 0, 6, 0, 0, 0, 0, 0],
+        ]
+    )
+    loaded_item = load_puzzle("tests/test_puzzles/valid.txt")
+
+    assert np.array_equal(loaded_item, correct_puzzle)
