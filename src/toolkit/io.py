063a986bba42af1ade9e457d33ee95e1ab0ff8c5
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-24 19:41:28 +0000

    Changed name of "validate_form" to "parse_sudoku_string" and moved it to io.py
    Explanation: I felt this would improve readability, since the validate_form function
    is the main function that converts sudoku strings to numpy arrays, and a
    function that does this must always validate the format.

--- src/toolkit/io.py
+++ src/toolkit/io.py
@@ -5,11 +5,75 @@
 
 @author Created by William Knottenbelt
 """
+import numpy as np
+from src.toolkit.validation import validate_puzzle
 
-from src.toolkit.validation import validate_form, validate_puzzle
+
+def parse_sudoku_string(sudoku_str):
+    """!
+    @brief Validate the format of a Sudoku puzzle as a string, and parse into numpy array.
+
+    @details This function checks if the input string represents a valid Sudoku puzzle
+    format. A valid Sudoku format is defined as a 9x9 grid of numbers (0-9),
+    where rows are separated by newlines, and digits can be separated using
+    symbols | - + ,
+    If the input string is valid, we return an array containing the puzzle
+
+    Note:
+    The function does not check if the puzzle is solvable or if it adheres to
+    Sudoku rules beyond formatting. It strictly checks the structural format.
+
+    @param sudoku_str: str, The input string to be validated
+
+    @return tuple containing two elements:
+            - bool: True if the string has a valid format, False otherwise
+            - If True, second element is array containing puzzle. If False, element is error message
+    """
+    if not isinstance(sudoku_str, str):
+        raise ValueError("Parameter sudoku_str must be a string")
+
+    # remove separators (special characters), spaces and empty lines
+    sudoku_str = (
+        sudoku_str.replace("|", "").replace("+", "").replace("-", "").replace(",", "")
+    )
+    sudoku_str = sudoku_str.replace(" ", "")
+    sudoku_str = sudoku_str.replace("\n\n", "\n")
+
+    # convert to list of rows
+    rows = sudoku_str.split("\n")
+    rows = [row for row in rows if row != ""]  # remove empty rows
+
+    # check that there are now 9 rows,
+    # each with exactly 9 characters
+    # and the 9 characters are all digits [0-9]
+    n_rows = len(rows)
+    if n_rows != 9:
+        return (
+            False,
+            f"Number of rows containing digits must be 9, but {n_rows} were given.",
+        )
+
+    for row in rows:
+        # checking for non-digit characters left
+        for char in row:
+            if not char.isdigit():
+                return (False, f"Found unrecognised character '{char}'")
+
+        # checking if there are 9 digits per row
+        n_digits = len(row)
+        if n_digits > 9:
+            return (False, "Too many digits on one or more rows.")
+        if n_digits < 9:
+            return (False, "Not enough digits on one or more rows.")
+
+    # convert to array
+    puzzle = np.array([[int(char) for char in row] for row in rows])
+    assert puzzle.shape == (9, 9)  # ensure shape is 9x9
+
+    return (True, puzzle)
 
 
-def load_puzzle(filepath):
+def load_puzzle(filepath, check_validity=True):
     """!@brief Function loads sudoku puzzles from text files to numpy arrays.
 
     @details
@@ -26,9 +90,9 @@ def load_puzzle(filepath):
     with open(filepath, "r") as file:
         text = file.read()
 
-    # Validate format of contents
+    # validate format of contents
     # if valid format, output is 9x9 array containing puzzle
-    valid_format, output = validate_form(text)
+    valid_format, output = parse_sudoku_string(text)
     if valid_format:
         # output is array containing puzzle
         puzzle = output
@@ -37,12 +101,13 @@ def load_puzzle(filepath):
         print(f"File contains INVALID FORMAT: {output}")
         return 0
 
-    # Validate that puzzle conforms to sudoku rules
-    # if invalid, returns error message
-    message = validate_puzzle(puzzle)
-    if message != "Valid":
-        print(f"Puzzle does not comply to sudoku rules: {message}")
-        return 0
+    if check_validity:
+        # validate that puzzle conforms to sudoku rules
+        # if invalid, returns error message
+        message = validate_puzzle(puzzle)
+        if message != "Valid":
+            print(f"Puzzle does not comply to sudoku rules: {message}")
+            return 0
 
     return puzzle
 
