389ecf68713474c579b7e014742ef5d973ce8003
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-24 20:10:32 +0000

    Moved test_validate_form to test_io.py and renamed to test_parse_sudoku_string
    We changed validate_form function to parse_sudoku_string and put it in
    io.py, hence we need to do the same for the test

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,6 +1,67 @@
 import numpy as np
-from src.toolkit.io import load_puzzle, save_puzzle, puzzle_to_string
+from src.toolkit.io import (
+    load_puzzle,
+    save_puzzle,
+    puzzle_to_string,
+    parse_sudoku_string,
+)
 import pytest
+
+
+def test_parse_sudoku_string():
+    """
+    Test parse_sudoku_string
+    """
+
+    # test it works for valid puzzle with valid format
+    valid_form1 = "tests/test_puzzles/valid.txt"
+    puzzle1 = np.array(
+        [
+            [0, 0, 0, 0, 0, 7, 0, 0, 0],
+            [0, 0, 0, 0, 0, 9, 5, 0, 4],
+            [0, 0, 0, 0, 5, 0, 1, 6, 9],
+            [0, 8, 0, 0, 0, 0, 3, 0, 5],
+            [0, 7, 5, 0, 0, 0, 2, 9, 0],
+            [4, 0, 6, 0, 0, 0, 0, 8, 0],
+            [7, 6, 2, 0, 8, 0, 0, 0, 0],
+            [1, 0, 3, 9, 0, 0, 0, 0, 0],
+            [0, 0, 0, 6, 0, 0, 0, 0, 0],
+        ]
+    )
+    with open(valid_form1, "r") as file:
+        text = file.read()
+        valid, output = parse_sudoku_string(text)
+        assert valid
+        assert np.array_equal(output, puzzle1)
+
+    # test it works for puzzle with invalid format
+    invalid_form1 = "tests/test_puzzles/invalid_form.txt"
+    with open(invalid_form1, "r") as file:
+        text = file.read()
+        valid, output = parse_sudoku_string(text)
+        assert not valid
+        assert output == "Too many digits on one or more rows."
+
+    # test it works for invalid puzzle with valid format
+    valid_form2 = "tests/test_puzzles/invalid_puzzle.txt"
+    puzzle2 = np.array(
+        [
+            [0, 0, 0, 0, 0, 7, 5, 0, 0],  # notice 5 is used twice in the 7th column
+            [0, 0, 0, 0, 0, 9, 5, 0, 4],
+            [0, 0, 0, 0, 5, 0, 1, 6, 9],
+            [0, 8, 0, 0, 0, 0, 3, 0, 5],
+            [0, 7, 5, 0, 0, 0, 2, 9, 0],
+            [4, 0, 6, 0, 0, 0, 0, 8, 0],
+            [7, 6, 2, 0, 8, 0, 0, 0, 0],
+            [1, 0, 3, 9, 0, 0, 0, 0, 0],
+            [0, 0, 0, 6, 0, 0, 0, 0, 0],
+        ]
+    )
+    with open(valid_form2, "r") as file:
+        text = file.read()
+        valid, output = parse_sudoku_string(text)
+        assert valid
+        assert np.array_equal(output, puzzle2)
 
 
 def test_load_puzzle():
