9c5b5c291e944790d457483f8f61c7d9b4b849f9
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-26 13:02:42 +0000

    Moved tests in test_solvers.py to test_engine.py
    I think one file is sufficient to test the engine functionality

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -2,6 +2,24 @@ from src.toolkit.validation import validate_solution
 from src.toolkit.io import load_puzzle
 
 from src.engine.basics import singles_filler
+from src.engine.backtracking import backtracker
+
+
+def test_backtracker():
+    # test for valid puzzle
+    filepath = "tests/test_puzzles/easy/easy_01.txt"
+    valid_puzzle = load_puzzle(filepath)
+
+    solution = backtracker(valid_puzzle)  # solve puzzle
+
+    assert validate_solution(valid_puzzle, solution) == "Valid"
+
+    # test for unsolvable puzzle
+    filepath = "tests/test_puzzles/unsolvable/unsolvable_01.txt"
+    unsolvable_puzzle = load_puzzle(filepath, check_validity=False)
+    solution = backtracker(unsolvable_puzzle)
+
+    assert solution == "UNSOLVABLE"
 
 
 def test_singles_filler():
