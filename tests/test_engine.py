f919d362d14a07923fb07dfa6ae1722c1604b7f9
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-26 15:40:17 +0000

    Created robust test to test ALL solvers (testing led development)
    When I create a new solver, I can simply add it to the list of
    solvers at the top of test_engine.py, then this test will run
    on that solver too. For now, I have commented out the testing on
    the hard tests, since backtracking takes too long to find these
    solutions, hence it is not feasible to run this test prior to
    every commit.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,6 +4,8 @@ from src.toolkit.io import load_puzzle
 from src.engine.basics import singles_filler
 from src.engine.backtracking import backtracker
 
+solvers = [backtracker]
+
 
 def test_backtracker():
     # test for valid puzzle
@@ -34,3 +36,31 @@ def test_singles_filler():
     hardest_puzzle = load_puzzle(filepath)
     filled = singles_filler(hardest_puzzle)
     assert validate_solution(hardest_puzzle, filled) == "Solution is Unfilled"
+
+
+def test_all_solvers_robustly():
+    """
+    This test validates all solvers on 6 puzzles:
+    - 3 easy puzzles
+    - 3 hard puzzles
+
+    The test is passed if all solvers solve all puzzles
+    """
+
+    easy_path = "tests/test_puzzles/easy/easy_"
+    # hard_path = "tests/test_puzzles/hard/hard_"
+
+    # 3 easy, hard and unsolvable puzzles
+    for end in ["01.txt", "02.txt", "03.txt"]:
+        # loading easy, hard and unsolvable puzzles
+        easy_puzzle = load_puzzle(easy_path + end)
+        #    hard_puzzle = load_puzzle(hard_path+end)
+
+        for solver in solvers:
+            # check solver solves easy puzzle correctly
+            solution = solver(easy_puzzle)
+            assert validate_solution(easy_puzzle, solution) == "Valid"
+
+            # check solver solves hard puzzle correctly
+        #    solution = solver(hard_puzzle)
+        #    assert validate_solution(hard_puzzle, solution) == "Valid"
