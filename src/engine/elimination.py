d8a1c699e4e8302961954d4551f619bcb8acd0c8
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-28 14:28:28 +0000

    Implemented naked_singles_elimination

--- src/engine/elimination.py
+++ src/engine/elimination.py
@@ -1,3 +1,37 @@
 """!
 @brief Module contains all functionality eliminating candidates from the candidates grid
 """
+import copy
+
+
+def naked_singles_elimination(candidates):
+    """
+    @brief Eliminate candidates using the naked singles technique.
+    """
+    candidates = copy.deepcopy(candidates)
+
+    for i in range(9):
+        for j in range(9):
+            if len(candidates[i, j]) == 1:
+                # extract the single candidate
+                value = list(candidates[i, j])[0]
+
+                # eliminate this candidate from all squares in the same row
+                for c in range(9):
+                    if c != j:
+                        candidates[i, c].discard(value)
+
+                # eliminate this candidate from all squares in the same column
+                for r in range(9):
+                    if r != i:
+                        candidates[r, j].discard(value)
+
+                # eliminate this candidate from all squares in the same block
+                block_i = 3 * (i // 3)  # row index of top left square in block
+                block_j = 3 * (j // 3)  # col index of top left square in block
+                for r in range(block_i, block_i + 3):
+                    for c in range(block_j, block_j + 3):
+                        if r != i or c != j:
+                            candidates[r, c].discard(value)
+
+    return candidates
--- tests/test_elimination.py
+++ tests/test_elimination.py
@@ -4,18 +4,27 @@ from src.engine.elimination import naked_singles_elimination
 
 
 def test_naked_singles_elimination():
+    # candidates for empty array (all numbers for all squares)
     puzzle = np.zeros((9, 9), dtype=int)
     candidates = init_candidates(puzzle)
-    # Manually set naked single
+
+    # manually insert naked single
     candidates[0, 0] = {3}
 
     candidates = naked_singles_elimination(candidates)
 
-    # assert 3 not in first row, column or block
+    # union of all candidates in the rest of the row and column
     rest_of_row = set().union(*candidates[0, 1:])
     rest_of_col = set().union(*candidates[1:, 0])
-    rest_of_block = set().union(*candidates[0:2, 0:2].flatten())
 
+    # union of all candidates in the rest of the block
+    rest_of_block = set().union(
+        *candidates[1:2, 0],  # two squares below (0,0)
+        *candidates[0, 1:2],  # two squares right of (0,0)
+        *candidates[1:2, 1:2].flatten()  # 4 squares in rest of block
+    )
+
+    # assert that 3 has been eliminated from the rest of the row, column or block
     assert {3}.isdisjoint(rest_of_row), "Single not eliminated from rest of row"
     assert {3}.isdisjoint(rest_of_col), "Single not eliminated from rest of column"
     assert {3}.isdisjoint(rest_of_block), "Single not eliminated from rest of block"
