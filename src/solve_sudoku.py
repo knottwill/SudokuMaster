793ab6fa3243b5d375d647f14517279a2e34acdd
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-29 15:04:38 +0000

    Put all code in solve_sudoku.py inside a function
    Explanation: We must do this to do profiling with line_profiler

--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -13,67 +13,74 @@ assert len(sys.argv) < 3, "Too many files provided"
 assert len(sys.argv) == 2
 
 filepath = sys.argv[1]
-puzzle = load_puzzle(filepath)
 
-# if puzzle fails to load, stop here
-# we don't need to print anything, as load_puzzle will do this
-if puzzle is None:
-    sys.exit()
 
-orig_puzzle = puzzle.copy()  # taking copy in case puzzle is modified
-start = time()  # timing
+# @profile
+def run_solver():
+    puzzle = load_puzzle(filepath)
 
-# --------------------
-# Candidate Elimination
-# --------------------
+    # if puzzle fails to load, stop here
+    # we don't need to print anything, as load_puzzle will do this
+    if puzzle is None:
+        sys.exit()
 
-# initialise candidates grid
-candidates = init_candidates(puzzle)
+    orig_puzzle = puzzle.copy()  # taking copy in case puzzle is modified
+    start = time()  # timing
 
-# perform candidate elimination techniques
-candidates = all_elimination(candidates)
+    # --------------------
+    # Candidate Elimination
+    # --------------------
 
-# check if puzzle is solvable
-if not solvable(candidates):
-    print("Puzzle is Unsolvable")
-    sys.exit()
+    # initialise candidates grid
+    candidates = init_candidates(puzzle)
 
-# fill in puzzle as much as possible
-filled_puzzle = filler(puzzle, candidates)
-post_elimination = time()
+    # perform candidate elimination techniques
+    candidates = all_elimination(candidates)
 
-# check if solution has been already found
-message = validate_solution(puzzle, filled_puzzle)
-if message == "Valid":
-    print(f"Solution Found in {post_elimination - start: .3}s\n")
-    print("Using candidate elimination alone\n")
-    print_puzzle(filled_puzzle)
-    sys.exit()  # stop running if solution found
-assert (
-    message == "Solution is Unfilled"
-), f"Puzzle filled by candidate elimination is invalid: {message}"
+    # check if puzzle is solvable
+    if not solvable(candidates):
+        print("Puzzle is Unsolvable")
+        sys.exit()
 
-# ------------------------
-# Brute force (backtracking)
-# ------------------------
+    # fill in puzzle as much as possible
+    filled_puzzle = filler(puzzle, candidates)
+    post_elimination = time()
 
-# perform backtracking
-solution = backtracker(puzzle, candidates)
-post_backtracking = time()
+    # check if solution has been already found
+    message = validate_solution(puzzle, filled_puzzle)
+    if message == "Valid":
+        print(f"Solution Found in {post_elimination - start: .3}s\n")
+        print("Using candidate elimination alone\n")
+        print_puzzle(filled_puzzle)
+        sys.exit()  # stop running if solution found
+    assert (
+        message == "Solution is Unfilled"
+    ), f"Puzzle filled by candidate elimination is invalid: {message}"
 
-# check if puzzle is unsolvable
-if isinstance(solution, str) and solution == "UNSOLVABLE":
-    print("Puzzle is Unsolvable")
-    sys.exit()
+    # ------------------------
+    # Brute force (backtracking)
+    # ------------------------
 
-# assert original puzzle has not been modified
-assert np.array_equal(puzzle, orig_puzzle), "Original puzzle has been modified"
+    # perform backtracking
+    solution = backtracker(puzzle, candidates)
+    post_backtracking = time()
 
-# if we get here, solution must have been found
-message = validate_solution(puzzle, solution)
-assert message == "Valid", f"Solution incorrect: {message}"
+    # check if puzzle is unsolvable
+    if isinstance(solution, str) and solution == "UNSOLVABLE":
+        print("Puzzle is Unsolvable")
+        sys.exit()
 
-# print solution
-print(f"Solution Found in {post_backtracking - start: .3}s\n")
-print("Using candidate elimination and backtracking\n")
-print_puzzle(solution)
+    # assert original puzzle has not been modified
+    assert np.array_equal(puzzle, orig_puzzle), "Original puzzle has been modified"
+
+    # if we get here, solution must have been found
+    message = validate_solution(puzzle, solution)
+    assert message == "Valid", f"Solution incorrect: {message}"
+
+    # print solution
+    print(f"Solution Found in {post_backtracking - start: .3}s\n")
+    print("Using candidate elimination and backtracking\n")
+    print_puzzle(solution)
+
+
+run_solver()
