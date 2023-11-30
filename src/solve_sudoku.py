31b5caf7fc2916d50d1149346022e6f0833f456e
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-30 18:46:07 +0000

    Added functionality to find multiple solutions if desired and to save solutions
    Modified tests/test_solver.py to accomodate and test this new functionality.
    Specifically, we test that solve_sudoku can find multiple solutions, and
    we modified the other tests to delete the files created by running the solver.
    
    Potential issue: These tests would overwrite and delete files in
    solutions/ if they are of the same names as the files used in the
    tests. We will add a warning to README.md to specify this.

--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -2,78 +2,92 @@ import sys
 import numpy as np
 from time import time
 
-from toolkit.io import load_puzzle, print_puzzle
+from toolkit.io import load_puzzle, print_puzzle, save_puzzle
 from toolkit.validation import validate_solution
 from engine.basics import init_candidates, filler, solvable
 from engine.elimination import all_elimination
 from engine.backtracking import backtracker
 
 assert len(sys.argv) != 1, "No filepath provided"
-assert len(sys.argv) < 3, "Too many files provided"
-assert len(sys.argv) == 2
+assert len(sys.argv) < 4, "Too many arguments provided"
 
+# filepath is passed as argument
 filepath = sys.argv[1]
 
+# num_solutions is (optionally) passed as argument
+# otherwise, we take num_solutions = 1
+if len(sys.argv) == 3:
+    assert sys.argv[2].isdigit(), "2nd argument, <num_solutions>, must be an integer"
+    num_solutions = int(sys.argv[2])
+else:
+    num_solutions = 1
 
-# @profile
-def run_solver():
-    puzzle = load_puzzle(filepath)
+# load puzzle
+puzzle = load_puzzle(filepath)
 
-    # if puzzle fails to load, stop here
-    # we don't need to print anything, as load_puzzle will do this
-    if puzzle is None:
-        sys.exit()
+# filename (for saving the file after)
+filename = filepath.split(".txt")[0].split("/")[-1]
 
-    orig_puzzle = puzzle.copy()  # taking copy in case puzzle is modified
-    start = time()  # timing
+# if puzzle fails to load, stop here
+# we don't need to print error message, load_puzzle will do this
+if puzzle is None:
+    sys.exit()
 
-    # --------------------
-    # Candidate Elimination
-    # --------------------
+orig_puzzle = puzzle.copy()  # taking copy in case puzzle is modified
+start = time()  # timing
 
-    # initialise candidates grid
-    candidates = init_candidates(puzzle)
+# --------------------
+# Candidate Elimination
+# --------------------
 
-    # perform candidate elimination techniques
-    candidates = all_elimination(candidates)
+# initialise candidates grid
+candidates = init_candidates(puzzle)
 
-    # check if puzzle is solvable
-    if not solvable(candidates):
-        print("Puzzle is Unsolvable")
-        sys.exit()
+# perform candidate elimination techniques
+candidates = all_elimination(candidates)
 
-    # fill in puzzle as much as possible
-    filled_puzzle = filler(puzzle, candidates)
-    post_elimination = time()
+# check if puzzle is solvable
+if not solvable(candidates):
+    print("Puzzle is Unsolvable")
+    sys.exit()
 
-    # check if solution has been already found
-    message = validate_solution(puzzle, filled_puzzle)
-    if message == "Valid":
-        print(f"Solution Found in {post_elimination - start: .3}s\n")
-        print("Using candidate elimination alone\n")
-        print_puzzle(filled_puzzle)
-        sys.exit()  # stop running if solution found
-    assert (
-        message == "Solution is Unfilled"
-    ), f"Puzzle filled by candidate elimination is invalid: {message}"
+# fill in puzzle as much as possible
+filled_puzzle = filler(puzzle, candidates)
+post_elimination = time()
 
-    # ------------------------
-    # Brute force (backtracking)
-    # ------------------------
+# check if solution has been already found
+message = validate_solution(puzzle, filled_puzzle)
+if message == "Valid":
+    print(f"Solution Found in {post_elimination - start: .3}s\n")
+    print("Using candidate elimination alone\n")
+    print_puzzle(filled_puzzle)
+    savepath = "solutions/" + filename + "_solution.txt"
+    save_puzzle(savepath, filled_puzzle)
+    print(f"Solution saved in {savepath}")
+    sys.exit()  # stop running if solution found
+assert (
+    message == "Solution is Unfilled"
+), f"Puzzle filled by candidate elimination is invalid: {message}"
 
-    # perform backtracking
-    solution = backtracker(puzzle, candidates)
-    post_backtracking = time()
+# ------------------------
+# Brute force (backtracking)
+# ------------------------
 
-    # check if puzzle is unsolvable
-    if isinstance(solution, str) and solution == "UNSOLVABLE":
-        print("Puzzle is Unsolvable")
-        sys.exit()
+# perform backtracking
+solutions = backtracker(puzzle, candidates, num_solutions)
+post_backtracking = time()
 
-    # assert original puzzle has not been modified
-    assert np.array_equal(puzzle, orig_puzzle), "Original puzzle has been modified"
+# check if puzzle is unsolvable
+if isinstance(solutions, str) and solutions == "UNSOLVABLE":
+    print("Puzzle is Unsolvable")
+    sys.exit()
 
-    # if we get here, solution must have been found
+# assert original puzzle has not been modified
+assert np.array_equal(puzzle, orig_puzzle), "Original puzzle has been modified"
+
+# if we get here, solution(s) must have been found
+if num_solutions == 1:
+    solution = solutions
     message = validate_solution(puzzle, solution)
     assert message == "Valid", f"Solution incorrect: {message}"
 
@@ -81,6 +95,18 @@ def run_solver():
     print(f"Solution Found in {post_backtracking - start: .3}s\n")
     print("Using candidate elimination and backtracking\n")
     print_puzzle(solution)
+    savepath = "solutions/" + filename + "_solution.txt"
+    save_puzzle(savepath, solution)
+    print(f"Solution saved in {savepath}")
 
+else:
+    print(f"{len(solutions)} Solution(s) Found in {post_backtracking - start: .3}s\n")
+    for i, solution in enumerate(solutions):
+        message = validate_solution(puzzle, solution)
+        assert message == "Valid", f"Solution incorrect: {message}"
 
-run_solver()
+        # print & save solutions
+        print_puzzle(solution)
+        savepath = "solutions/" + filename + "_solution" + str(i + 1) + ".txt"
+        save_puzzle(savepath, solution)
+        print(f"Solution saved in {savepath}")
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -2,6 +2,7 @@
 Robust testing for solve_sudoku.py
 """
 import subprocess
+import os
 
 # path to solver script
 path_to_solver = "./src/solve_sudoku.py"
@@ -26,10 +27,37 @@ for file in ["01.txt", "02.txt", "03.txt"]:
 def test_solver_on_valid():
     # test on all valid test puzzles
     for filepath in valid_filepaths:
+        filename = filepath.split(".txt")[0].split("/")[-1]
         result = subprocess.run(
             ["python", path_to_solver, filepath], capture_output=True, text=True
         )
         assert result.stdout.startswith("Solution Found")
+
+        # remove file created
+        os.remove("solutions/" + filename + "_solution.txt")
+
+    # if 'solutions/' is now empty, delete it too
+    if not os.listdir("solutions/"):
+        os.rmdir("solutions/")
+
+
+def test_multiple_solution():
+    filepath = "tests/test_puzzles/10_solutions.txt"
+    num_solutions = "4"
+    result = subprocess.run(
+        ["python", path_to_solver, filepath, num_solutions],
+        capture_output=True,
+        text=True,
+    )
+    assert result.stdout.startswith("4 Solution(s) Found")
+
+    # remove files created
+    for n in range(1, 5):
+        os.remove("solutions/10_solutions_solution" + str(n) + ".txt")
+
+    # if 'solutions/' is now empty, delete it too
+    if not os.listdir("solutions/"):
+        os.rmdir("solutions/")
 
 
 def test_solver_on_unsolvable():
@@ -71,10 +99,10 @@ def test_solver_error_raising():
     )
     assert result.returncode != 0, "No errors raised when no arguments passed"
 
-    # too many arguments passed
+    # passing two files as arguments
     filepath1 = "tests/test_puzzles/easy/easy_01.txt"
     filepath2 = "tests/test_puzzles/easy/easy_02.txt"
     result = subprocess.run(
         ["python", path_to_solver, filepath1, filepath2], capture_output=True, text=True
     )
-    assert result.returncode != 0, "No errors raised when too many arguments passed"
+    assert result.returncode != 0, "No errors raised when passing two sudoku files"
